569a46e6b73eee02fb2bd6664d05c47d51c0fea1
omur aydin <ommuraydin@gmail.com>
2021-12-30 20:24:39 +0300

    map placed in the map

--- src/Pages/Activity/Activity.js
+++ src/Pages/Activity/Activity.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { View, Button, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Circle } from 'react-native-maps';
 
 const DashboardLayout = () => {
   return (
-    <View>
-      <Text>Start an activity!</Text>
-      <MapView
-        style={{ flex: 1, }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+    <View style={{ flex: 1, position: 'relative' }}>
+      <View style={{ flex: 1 }}
+        pointerEvents="none"
+      >
+        <MapView
+          style={{ flex: 1, opacity: 0.6 }}
+          minZoomLevel={18}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Circle center={{
+            latitude: 37.78825,
+            longitude: -122.4324
+          }} 
+          radius={4} 
+          fillColor="red"
+          />
+        </MapView>
+      </View>
+      <Button title="Go"></Button>
     </View >
   );
 }
