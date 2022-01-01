589390092c4b39836c599073dab4023cdf6cf442
omur aydin <ommuraydin@gmail.com>
2022-01-01 18:57:53 +0300

    Direction line has drawn between two markers

--- src/Pages/Activity/Activity.js
+++ src/Pages/Activity/Activity.js
@@ -1,91 +1,97 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Button, PermissionsAndroid, Platform } from 'react-native';
+import { View, Button, PermissionsAndroid, Platform, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import MapView, { Circle, Polyline, Marker, AnimatedRegion } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import { hasPermission } from "../../Hooks/useLocationPermission";
+import MapViewDirections from "react-native-maps-directions";
+
+const GOOGLE_API = "AIzaSyArbjnFtKZXprOc80XxdhqMQ7szz-AnBhM"
+
 
 const Activity = () => {
   const navigation = useNavigation();
-  const [position, setPosition] = useState(null)
+  const [currentPosition, setCurrentPosition] = useState(initialState)
+  const mapRef = useRef(null)
+
+  const initialState = {
+    latitude: 37.3318456,
+    longitude: -122.0296002,
+
+  }
+
+  const destination = {
+    latitude: 37.771707,
+    longitude: -122.4053769,
+  }
 
   const interval = useRef(null)
 
   useEffect(() => {
-    navigation.addListener("focus", event => {
-      interval.current = setInterval(() => getCurrentLocation(), 30000);
-      getCurrentLocation();
-    });
-    return () => clearInterval(interval.current)
-  }, [navigation])
+    if (!initialState || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+    })
+  }, [initialState, destination])
 
-  useEffect(() => {
-    navigation.addListener("blur", event => {
-      clearInterval(interval.current);
-      interval.current = null;
-    });
-    return () => clearInterval(interval.current)
-  }, [navigation])
-
-
-  const getCurrentLocation = async () => {
-    const locationPermission = await hasPermission();
-    if (!locationPermission)
-      return;
-    Geolocation.getCurrentPosition(
-      pos => {
-        const initialPosition = pos
-        setPosition(initialPosition)
-        console.log("POSİTİON", pos)
-      }, error => {
-        setPosition(null)
-        console.log("ERROR", error)
-      }, {
-      accuracy: {
-        android: "high",
-      },
-      enableHighAccuracy: true,
-      timeout: 30000,
-      maximumAge: 10000,
-      distanceFilter: 0,
-      forceRequestLocation: true,
-      forceLocationManager: false,
-      showLocationDialog: true,
-    }
-
-    )
-  }
-
+  console.log(destination.latitude)
   return (
     <View style={{ flex: 1, position: 'relative' }}>
       <View style={{ flex: 1 }}
         pointerEvents="none"
       >
         <MapView
+          ref={mapRef}
           style={{ flex: 1, opacity: 0.6 }}
-          minZoomLevel={18}
-          region={{
-            latitude: position?.coords?.latitude || 37.78825,
-            longitude: position?.coords?.longitude || -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+          initialRegion={{
+            latitude: initialState.latitude,
+            longitude: initialState.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
           }}
         >
-          <Polyline />
           <Circle
             center={{
-              latitude: position?.coords?.latitude || 37.78825,
-              longitude: position?.coords?.longitude || -122.4324
+              latitude: initialState.latitude,
+              longitude: initialState.longitude,
             }}
-            radius={4}
+            radius={10}
             fillColor="red"
           />
+          {initialState && destination && (
+            <MapViewDirections
+              origin={initialState}
+              destination={destination}
+              apikey={GOOGLE_API}
+              strokeWidth={3}
+              strokeColor="blue"
+            />
+          )}
+
+          {initialState && (
+            <Marker
+              coordinate={{
+                latitude: initialState.latitude,
+                longitude: initialState.longitude
+              }}
+              title="Origin"
+              description="Origin Marker"
+              identifier="origin"
+            />)}
+          {destination && (
+            <Marker
+              coordinate={{
+                latitude: destination.latitude,
+                longitude: destination.longitude
+              }}
+              title="Destination"
+              description="Destination Marker"
+              identifier="destination"
+            />)}
         </MapView>
       </View>
       <Button title="Go"></Button>
     </View >
-  );
+  )
 }
-
-
 export default Activity;
