dba597c6f6687de2b0eafc8aad8f5b63b45674c6
omur aydin <ommuraydin@gmail.com>
2022-01-04 02:22:36 +0300

    SignOut button placed

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 import DashboardLayout from './DashboardLayout';
+import auth from '@react-native-firebase/auth';
 
 const Dashboard = () => {
+
+  const handleSignOut = () => auth().signOut();
+
   return (
-    <DashboardLayout />
+    <DashboardLayout onSignOut={handleSignOut} />
   );
 }
 
--- src/Pages/Dashboard/DashboardLayout/DashboardLayout.js
+++ src/Pages/Dashboard/DashboardLayout/DashboardLayout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, Button, Text } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import routes from '../../../Navigation/routes';
+import auth from '@react-native-firebase/auth';
 
 
-const DashboardLayout = () => {
+const DashboardLayout = ({ onSignOut }) => {
   const navigation = useNavigation();
 
   const handleGoActivity = _ => {
-    navigation.navigate("YeniAktivite", );
+    navigation.navigate(routes.ACTIVITY_PAGE,);
 
   }
 
@@ -17,6 +19,7 @@ const DashboardLayout = () => {
       <Button title='Yeni Aktivite' onPress={handleGoActivity} />
       <Button title='Aktivite Geçmişi' />
       <Button title='Leaderboard' />
+      <Button title='Sign Out' onPress={onSignOut} />
     </View >
   );
 }
