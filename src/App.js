fe624f5e686a172d52f13d53ecbc8d6b2d65546f
omur aydin <ommuraydin@gmail.com>
2022-01-10 04:03:35 +0300

    splash screen added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import Navigation from "./Navigation";
+import SplashScreen from 'react-native-splash-screen'
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+  
   return (
     <View style={{ flex: 1 }}>
       <Navigation />
